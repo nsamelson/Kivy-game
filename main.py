9db0eefb23c88bd7f6f54000a64551ae125ef7da
wasanico <salocin.sam@gmail.com>
2019-12-08 13:41:26 +0100

    saving the score in a json file

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-from kivy.uix.behaviors import ToggleButtonBehavior
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.button import Button
 from kivy.properties import (StringProperty, ObjectProperty, OptionProperty,NumericProperty, ListProperty)
-from kivy.factory import Factory
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.core.window import Window
 
+import json
+import os
 
 Alpha =["A","B","C","D","E","F","G","H","I","J"]
 Wreck=[]
 Copy=[]
 Sunk=[]
 Sunkships =[]
-
+Bingo = []
 Carrier = ["B2","B3","B4","B5","B6"]
 Cruiser = ["A9","B9","C9","D9"]
 Submarine1 = ["H8","I8","J8"]
@@ -33,24 +31,16 @@ class FirstWindow (Screen):
 class SecondWindow (Screen):
     pass
 class ThirdWindow (Screen):
-    def on_leave(self): # Trying to reset the game so I can replay it without quitting and restarting
-
-        self.clear_widgets()
-    def on_enter(self):
-       # self.add_widget(Counter.counter((self, counter.moves()), score ,sink))
-        Counter.counter(self,moves=0,score=0,sink="")   # I don't know what to put
-
-
-class FourthWindow (Screen):
     pass
 
+class FourthWindow (Screen):
+    def on_pre_enter(self):
+        Window.size = (1150, 500)
+    def on_pre_leave(self):
+        Window.size = (800, 600)
+
 class Counter(GridLayout):
-    def counter(self, moves, score,sink):
-        sink.text = str("\n".join(Sunkships))   #It's for telling which ship has been sunk. I have also an issue with it where I must click another button to activate it and show the ship that sank
-        moves.text = (str(int(moves.text) + 1))
-        score.text = str("%.2f"% (1700 / int(moves.text)) if int(moves.text) > 17 else str("%.2f"%(100 * int(moves.text) / 17)))
-
-
+    pass
 
 
 class MyWidget(GridLayout):
@@ -67,13 +57,19 @@ class MyWidget(GridLayout):
             for i in range(10):
                 self.box.add_widget(ImageButton(id=str(Alpha[j])+str(i+1)))
 
+    def reset_board(self):
+        image_buttons = [widget for widget in self.box.walk() if isinstance(widget, ImageButton)]
+        for image_button in image_buttons:
+            image_button.reset()
+
 class ImageButton(ButtonBehavior,Image):
     def __init__(self, **kwargs):
         super(ImageButton, self).__init__(**kwargs)
         self.source = 'blue.png'
 
-    #box = ObjectProperty(None) pour essayer de clear la grille
-
+    def reset(self):
+        self.source = 'blue.png'
+        self.disabled = False
 
     def on_press(self):
         self.source = "blue2.png"
@@ -86,6 +82,7 @@ class ImageButton(ButtonBehavior,Image):
                     ord = ship.index(xy)
                     ship.pop(ord)
                     Wreck.append(xy)
+                    Bingo.append(xy)
             if len(ship) == 0:
                 Copy = Wreck.copy()
                 Sunk.append(Copy)
@@ -102,46 +99,71 @@ class ImageButton(ButtonBehavior,Image):
                 Ships.pop(sunk)
 
 
-
     def on_release(self):
         if len(Ships) ==0:
             Sunkships.clear()
             Sunkships.append("Touché Coulé")
             print("Touché coulé")
-
             self.popopen()
-            #self.clear_party()
-
+            Wreck.clear()
+            Copy.clear()
+            Sunk.clear()
+            Sunkships.clear()
+            Bingo.clear()
     def popopen(self):
         popsave = Pop()
         popsave.open()
-        #scores.text = Counter.counter.score.text
-    def clear_party(self):
-
-        MyWidget.__init__(self)
-
-
-
-
-
-            #save_score()               #trying to show the popup to save score
-
 
 class Pop (Popup):
-    pass
+    name = ObjectProperty(None)
+    bridgename = ObjectProperty(None)
+    def savebridge(self):
+        print(self.bridgename.text)
+        self.name = str(self.bridgename)
+        print(self.name)
 
-kv = Builder.load_file("my.kv")
 
 class MyMainApp (App):
-    def build(self):
-        return kv
+    moves = StringProperty('0')
+    score = StringProperty('0')
+    sink = StringProperty('ships touched')
+    bingo = StringProperty("0")
+    #name = ObjectProperty(None)
+    def counter(self):
+        self.sink = str("\n".join(Sunkships))   #It's for telling which ship has been sunk. I have also an issue with it where I must click another button to activate it and show the ship that sank
+        self.moves = (str(int(self.moves) + 1))
+        self.bingo = str(len(Bingo)+1)
+        self.score = str("%.2f"% (100*int(self.bingo) / int(self.moves)))
 
+    def btnsolo(self):
+        print("solo")
+    def btnduel(self):
+        print("duel")
 
+    def savescore(self, filename):
 
-    def save_score(self):
-        pass
+        jsonresult = {'player': self.name, 'score': self.score}
+
+        print(jsonresult)
+
+        if (os.path.isfile(filename)):
+            # lecture des scores
+            with open(filename, "r") as file:
+                data = json.load(file)
+        else:
+            data = {}
+            data['scores'] = []
+
+        # ajout d'un nouveau score
+        data['scores'].append({'player': self.name, 'score': self.score})
+
+        with open(filename, "w") as file:
+            json.dump(data, file)
+            file.close()
+        self.name.text = ""
     def schow_score(self):
         pass
 
 if __name__ == "__main__":
     MyMainApp().run()
+    FileName ="C:/Users/nisam/Documents/ECAM/info/Battleship game v5.0/result.json"
